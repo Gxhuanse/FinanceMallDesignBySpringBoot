aa5fa3e890a87327db6b54c6ff4793e1a62dca67
gxhuan <234@qq.com>
2023-09-27 14:18:58 +0800

    MP防全表更新插件添加 MVC配置添加静态资源路径映射 用户修改头像功能实现 配置文件添加上传文件大小限制 登录页面地址端口修正 类别查询条件增加推荐和上架

--- src/main/java/com/gxh/config/WebConfig.java
+++ src/main/java/com/gxh/config/WebConfig.java
@@ -3,8 +3,10 @@ package com.gxh.config;
 import com.gxh.controller.interceptors.MyFstInterceptor;
 import com.gxh.controller.interceptors.MySecInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -17,6 +19,24 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private MySecInterceptor mySecInterceptor;
+
+    @Value("${upload.path}")
+    private String path;
+
+    /*关于图片上传后需要重启服务器才能刷新图片
+    这是一种保护机制，为了防止绝对路径被看出来，目录结构暴露
+    解决方法:将虚拟路径/images/
+    向绝对路径 (path)
+    映射*/
+    /**
+     * 添加静态资源的映射路径
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 添加静态资源映射路径
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:" + path);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
